22ab8da416176e82c1bde5c732ce6444ed9bf324
annapalaeva <palaevaann@gmail.com>
2024-11-22 10:31:32 +0300

    вот fsidodvasoapfiop

--- splar_main.py
+++ splar_main.py
@@ -78,7 +78,7 @@ def open_file_dialog():
     perform_execution = False
     for obj in space_objects:
         space.delete(obj.image)  # удаление старых изображений планет
-    in_filename = askopenfilename(filetypes=(("Text file", ".txt"),))
+    in_filename = askopenfilename(filetypes=(("solar_system", ".txt"),))
     space_objects = read_space_objects_data_from_file(in_filename)
     max_distance = max([max(abs(obj.x), abs(obj.y)) for obj in space_objects])
     calculate_scale_factor(max_distance)
